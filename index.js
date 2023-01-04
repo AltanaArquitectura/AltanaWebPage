bd5ac10f34bae9907639db8d0a26374ceb734f92
MarcoCabort <marco.cabeza.ortega@gmail.com>
2023-01-04 16:07:48 +0100

    pruebas tabla

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
+
+function setStyle() {
+    /* Obtiene el ancho de la pantalla */
+    var screenWidth = window.innerWidth;
+  
+    /* Selecciona todas las celdas de la tabla */
+    var cells = document.querySelectorAll("td");
+  
+    /* Si la pantalla es más grande de 800px, establece el ancho de las celdas como el 33.3333% del ancho de la tabla */
+    if (screenWidth > 800) {
+      cells.forEach(function(cell) {
+        cell.style.width = "33.3333%";
+      });
+    }
+    /* Si la pantalla es más pequeña de 800px, establece el ancho de las celdas como el 100% del ancho de la tabla */
+    else {
+      cells.forEach(function(cell) {
+        cell.style.width = "100%";
+      });
+    }
+  }
+  
+  /* Ejecuta la función setStyle cuando se redimensiona la ventana del navegador */
+  window.addEventListener("resize", setStyle);
+  
+  /* Ejecuta la función setStyle cuando se carga la página */
+  window.addEvent
+  
